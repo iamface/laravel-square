7106acf901bae379854054792a7934cc18f3790f
Jared Ivory <iamface@jaredivory.com>
2017-06-06 12:52:50 -0600

    init controller and service provider

---
+++ src/LaravelSquareController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Iamface\LaravelSquare;
+
+use App\Http\Controllers\Controller;
+
+class LaravelSquareController extends Controller
+{
+    public function index()
+    {
+
+    }
+}
