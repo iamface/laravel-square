ed380c16ba2ef07390bc860b02b373cc2909eccf
Jared Ivory <iamface@jaredivory.com>
2017-06-29 09:26:58 -0600

    todos

--- src/LaravelSquareController.php
+++ src/LaravelSquareController.php
@@ -3,6 +3,7 @@
 namespace Iamface\LaravelSquare;
 
 use App\Http\Controllers\Controller;
+use SquareConnect\Api\CustomersApi;
 use SquareConnect\Api\LocationsApi;
 use SquareConnect\Configuration;
 
@@ -26,6 +27,45 @@ class LaravelSquareController extends Controller
     public function index()
     {
         dd($this->_locations);
+    }
+
+    public function getCustomers($locationId = null) {
+        // TODO return all customer records
+    }
+
+    public function getCustomer() {
+        // TODO return a specific customer record
+    }
+
+    public function authorizeCard() {
+        // TODO authorize card for transaction
+    }
+
+    public function listAuthorizations() {
+        // TODO return a list of authorizations by location
+    }
+
+    private function _saveCard() {
+        // TODO save card on file
+    }
+
+    private function _removeCard() {
+        // TODO remove card on file
+    }
+
+    /**
+     * Returns an array of customer records
+     *
+     * @return \SquareConnect\Model\Customer[] {Array}
+     */
+    private function _getCustomers() {
+        $customersAPI = new CustomersApi();
+
+        return $customersAPI->listCustomers()->getCustomers();
+    }
+
+    private function _saveCustomer() {
+        // TODO save new customer
     }
 
     /**
