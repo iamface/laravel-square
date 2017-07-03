b72083fb80848f88f1bf3911331018ad1d5b00ba
Jared Ivory <iamface@jaredivory.com>
2017-07-03 12:09:39 -0600

    location filter config

--- src/config.php
+++ src/config.php
@@ -21,6 +21,19 @@ return [
     | Default Currency
     |--------------------------------------------------------------------------
      */
-    'currency' => 'USD'
+    'currency' => 'USD',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Include Non-Capable Locations
+    |--------------------------------------------------------------------------
+    |
+    | This value determines if all locations will be returned (including non-capable
+    | credit card processing locations). Only credit card processing capable locations
+    | will be able to create authorizations and transactions. If you still want to
+    | return and list locations that cannot process cards for display only, set
+    | this to true.
+     */
+    'non_capable_locations' => false
 
 ];
