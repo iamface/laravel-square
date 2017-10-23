c4ae6f89e558a58a6bf1b72a2e244e0678cdfbb4
Jared Ivory <iamface@jaredivory.com>
2017-10-23 13:40:02 -0600

    add some todos and routes

--- src/routes.php
+++ src/routes.php
@@ -2,7 +2,19 @@
 
 Route::prefix('laravelsquare')->group(function () {
     Route::get('index', 'iamface\laravelsquare\LaravelSquareController@index');
+
+    // List customers
     Route::get('customers/{info?}', 'iamface\laravelsquare\LaravelSquareController@getCustomers');
+
+    // Get customer
     Route::get('customer/{identifier}', 'iamface\laravelsquare\LaravelSquareController@getCustomer');
+
+    // Authorize card
     Route::post('authorize', 'iamface\laravelsquare\LaravelSquareController@authorizeCard');
+
+    // List transactions
+    Route::get('transactions/{identifier}', 'iamface\laravelsquare\LaravelSquareController@listTransactions');
+
+    // List locations
+    Route::get('locations', 'iamface\laravelsquare\LaravelSquareController@listLocations');
 });
